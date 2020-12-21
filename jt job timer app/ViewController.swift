058798642875822fcc529318d8ce2c37766f59b6
Alberto Giambone <alberto.giambone@icloud.com>
2020-12-21 11:20:21 +0100

    Initial Commit

---
+++ jt job timer app/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  jt job timer app
+//
+//  Created by Alberto Giambone on 21/12/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
